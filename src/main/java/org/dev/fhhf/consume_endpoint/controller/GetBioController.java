1f1bc2e4fbfe068703c9afdd54135815d6498fe3
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-01 22:01:16 +0300

    Refactor endpoint | allowed CrossOrigin

--- src/main/java/org/dev/fhhf/consume_endpoint/controller/GetBioController.java
+++ src/main/java/org/dev/fhhf/consume_endpoint/controller/GetBioController.java
@@ -4,17 +4,17 @@ import org.dev.fhhf.consume_endpoint.model.Bio;
 import org.dev.fhhf.consume_endpoint.service.ConsumeApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/api")
 public class GetBioController {
 
     @Autowired
     private ConsumeApi consumeApi;
 
-    @GetMapping("{userName}/Bio")
+    @GetMapping("/bio/{userName}")
     public ResponseEntity<Bio> getBioByUserName(@PathVariable("userName") String userName) {
         Bio bio = consumeApi.getBioByUserName(userName);
         return ResponseEntity.ok(bio);
